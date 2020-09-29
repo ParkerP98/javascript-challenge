8b3c9dd3566434691879c5027ba57432cf0b6ec5
Parker Prowell <Parker.prowell@gmail.com>
2020-09-28 23:30:35 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -17,3 +17,29 @@ function createTable(ufoData) {
 };
 // Function call //
 createTable(tableData);
+
+// assign button to d3
+var button = d3.select("#filter-btn");
+
+
+button.on('click', function(){
+    // select the input element and get the html node
+    var input = d3.select('.form-control');
+    // get input value
+    var inputD = input.property('value');
+    // filter data based on input date
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputD);
+    // select table body
+    var tbody = d3.select('tbody')
+    // clean the table body
+    tbody.html('');
+
+    // I may be able to use my createTable function, but for some reason it wasn't working
+    //loop through populate tbody with filtered data
+    filteredData.forEach(function(ufo){
+        var row = tbody.append('tr');
+        Object.entries(ufo).forEach(function([key, value]){
+            var cell = row.append('td').text(value);
+        })
+    })
+});
